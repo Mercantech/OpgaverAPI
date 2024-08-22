1c2c711f69db0f2cc83e2d59ee51419f1901d65e
Dit Navn <dinemail@eksempel.com>
2024-08-22 07:25:04 +0000

    Deploy ready - Add EFCore

---
+++ Controllers/Opgaver.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text.Json;
+
+namespace OpgaverAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OpgaverController : ControllerBase
+    {
+        [HttpGet("Diesel")]
+        public IActionResult GetDiesel()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "JSON", "diesel.json");
+            var jsonData = System.IO.File.ReadAllText(filePath);
+            var dieselData = JsonSerializer.Deserialize<object>(jsonData);
+
+            return Ok(dieselData);
+        }
+        [HttpGet("Miles95")]
+        public IActionResult GetMiles95()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "JSON", "miles95.json");
+            var jsonData = System.IO.File.ReadAllText(filePath);
+            var dieselData = JsonSerializer.Deserialize<object>(jsonData);
+
+            return Ok(dieselData);
+        }
+        [HttpGet("Bilbasen")]
+        public IActionResult GetBilbasen()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "JSON", "bilbasen.json");
+            var jsonData = System.IO.File.ReadAllText(filePath);
+            var dieselData = JsonSerializer.Deserialize<object>(jsonData);
+
+            return Ok(dieselData);
+        }
+    }
+}
