492d4315b594a8477334e27bd89a2aa0188aa2aa
MAGS <mags@mercantec.dk>
2024-11-25 20:24:58 +0000

    Small

--- Program.cs
+++ Program.cs
@@ -38,11 +38,9 @@ namespace OpgaverAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
 
             app.UseHttpsRedirection();
 
